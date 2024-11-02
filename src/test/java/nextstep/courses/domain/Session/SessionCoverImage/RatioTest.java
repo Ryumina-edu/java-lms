37acdaf5755b7ff58968003c36f6e3afc883b922
Ryumina-edu <fbalsk8897@gmail.com>
2024-11-03 00:41:53 +0900

    refactor: RatioTest 정상 케이스 추가

--- src/test/java/nextstep/courses/domain/Session/SessionCoverImage/RatioTest.java
+++ src/test/java/nextstep/courses/domain/Session/SessionCoverImage/RatioTest.java
@@ -7,6 +7,16 @@ import org.junit.jupiter.api.Test;
 class RatioTest {
 
     @Test
+    void create_올바른_비율인_경우() {
+        Width width = new Width(300);
+        Height height = new Height(200);
+
+        Ratio ratio = new Ratio(width, height);
+
+        Assertions.assertThat(ratio).isNotNull();
+    }
+
+    @Test
     @DisplayName("가로와 세로 비율이 올바르지 않은 경우 IllegalArgumentException 가 발생한다.")
     void create_올바른_비율이_아닌_경우() {
         Width width = new Width(400);
