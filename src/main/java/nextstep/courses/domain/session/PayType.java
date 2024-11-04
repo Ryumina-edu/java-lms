8898cae3d1d50891f529c82c4c15da2a96b0c7d5
Ryumina-edu <fbalsk8897@gmail.com>
2024-11-04 12:35:05 +0900

    refactor: 가독성을 위한 부정문 -> 긍정문 변경

--- src/main/java/nextstep/courses/domain/session/PayType.java
+++ src/main/java/nextstep/courses/domain/session/PayType.java
@@ -8,12 +8,16 @@ public enum PayType {
             throw new IllegalArgumentException("무료 강의는 0원이어야 합니다.");
         }
 
-        if (!isFree() && price == 0) {
+        if (isPay() && price == 0) {
             throw new IllegalArgumentException("유료 강의는 0원 이상이어야 합니다.");
         }
     }
 
     private boolean isFree() {
         return this == FREE;
+    }
+
+    private boolean isPay() {
+        return this == PAY;
     }
 }
