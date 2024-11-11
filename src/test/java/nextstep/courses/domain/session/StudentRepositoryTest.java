8d5ae3089d804f8166c1e777986b78cfc66a21a7
Ryumina-edu <fbalsk8897@gmail.com>
2024-11-11 14:45:01 +0900

    refactor: drop table 구문 및 불필요 소스 제거

--- src/test/java/nextstep/courses/domain/session/StudentRepositoryTest.java
+++ src/test/java/nextstep/courses/domain/session/StudentRepositoryTest.java
@@ -8,13 +8,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @JdbcTest
 class StudentRepositoryTest {
 
